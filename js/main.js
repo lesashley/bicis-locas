195f6d49b6cbff2c988def236fb97b38466abea1
Leslie A. Lopez <lesashley56@gmail.com>
2017-03-02 16:32:15 -0500

    Avance en Laboratoria

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
-function validateForm(){
-	/* Escribe tú código aquí */
+document.getElementById("enviar").onclick = function validateForm(){
+	var name = document.getElementById("name").value;
+  var lastname = document.getElementById("lastname").value;
+  var email = document.getElementById("input-email").value;
+  var password = document.getElementById("input-password").value;
+  var bici = document.getElementById("bici").selectedIndex;
+  var social = document.getElementById("input-social").value;
+
+  if (name == null || name.length == 0 || !/^\s+$/.test(name)){
+    alert("Debe escribir un nombre válido");
+  }
+
+  if (lastname == null || lastname.length == 0 || /^\s+$/.test(lastname)){
+    alert("Debe escribir un apellido válido");
+  }
+
+  if(!(/\S+@\S+\.\S+/.test(email))){
+			alert('Debe escribir un correo válido');
+	}
+/*
+  if (password == null || password.length == 0 || {6}.test(password)){
+    alert("Debe escribir una contraseña válida");
+    else if(password == "password" || password == "123456" || password =="098754"){
+      alert("Contraseñas inválidas");
+    }
+  }
+  */
+
+
+
+
+  if( bici == null || bici == 0 ) {
+    alert("Completar datos en seleccionar bici");
+  }
+
+
+
 }
