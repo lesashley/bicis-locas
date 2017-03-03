f23a1ecdbba3111e0b0ec6f11d03c1dec002792e
Leslie A. Lopez <lesashley56@gmail.com>
2017-03-03 12:48:42 -0500

    Bicis-locas Validado

--- js/main.js
+++ js/main.js
@@ -1,38 +1,36 @@
-document.getElementById("enviar").onclick = function validateForm(){
+function validateForm(){
 	var name = document.getElementById("name").value;
   var lastname = document.getElementById("lastname").value;
   var email = document.getElementById("input-email").value;
   var password = document.getElementById("input-password").value;
   var bici = document.getElementById("bici").selectedIndex;
   var social = document.getElementById("input-social").value;
-
-  if (name == null || name.length == 0 || !/^\s+$/.test(name)){
-    alert("Debe escribir un nombre válido");
+//Validaciones en nombre
+  if (name == null || name.length == 0 || !(/^[A-Z][a-z]*/.test(name))){
+		if (name.charAt(0).toUpperCase()!= name.charAt(0)){
+			alert ("Debe colocar mayúscula en nombre");
+		}else{
+		alert("Debe escribir un nombre válido");}
   }
-
-  if (lastname == null || lastname.length == 0 || /^\s+$/.test(lastname)){
-    alert("Debe escribir un apellido válido");
+	//Validaciones en apellido
+	if (lastname == null || lastname.length == 0 || !(/^[A-Z][a-z]*/.test(lastname))){
+		if (lastname.charAt(0).toUpperCase()!= lastname.charAt(0)){
+			alert ("Debe colocar mayúscula en apellido");
+		}else{
+		alert("Debe escribir un apellido válido");}
   }
-
-  if(!(/\S+@\S+\.\S+/.test(email))){
+	//
+  if(!(/\S+@\S+\.\S+/.test(email)) || email == null || email.length == 0){
 			alert('Debe escribir un correo válido');
 	}
-/*
-  if (password == null || password.length == 0 || {6}.test(password)){
-    alert("Debe escribir una contraseña válida");
-    else if(password == "password" || password == "123456" || password =="098754"){
-      alert("Contraseñas inválidas");
-    }
+  if (password == null || password.length == 0){
+		alert("Debe escribir una contraseña válida");
   }
-  */
-
-
-
-
+	if(password.toString().length < 6 || password.toString() == "password" ||
+ 			password == "123456" || password == "098754"){
+		alert("Contraseña incorrecta");
+	}
   if( bici == null || bici == 0 ) {
     alert("Completar datos en seleccionar bici");
   }
-
-
-
 }
