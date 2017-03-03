a665bb578e93a0ed9a2465b6d842f40b8cc5c7e7
Leslie A. Lopez <lesashley56@gmail.com>
2017-03-03 18:05:07 -0500

    Validando espacios

--- js/main.js
+++ js/main.js
@@ -5,32 +5,33 @@ function validateForm(){
   var password = document.getElementById("input-password").value;
   var bici = document.getElementById("bici").selectedIndex;
   var social = document.getElementById("input-social").value;
+
+	var expreLetras = /([a-zñáéíóú ]+)$/;
 //Validaciones en nombre
-  if (name == null || name.length == 0 || !(/^[A-Z][a-z]*/.test(name))){
-		if (name.charAt(0).toUpperCase()!= name.charAt(0)){
-			alert ("Debe colocar mayúscula en nombre");
-		}else{
-		alert("Debe escribir un nombre válido");}
-  }
-	//Validaciones en apellido
-	if (lastname == null || lastname.length == 0 || !(/^[A-Z][a-z]*/.test(lastname))){
-		if (lastname.charAt(0).toUpperCase()!= lastname.charAt(0)){
-			alert ("Debe colocar mayúscula en apellido");
-		}else{
-		alert("Debe escribir un apellido válido");}
-  }
-	//
-  if(!(/\S+@\S+\.\S+/.test(email)) || email == null || email.length == 0){
+  	if (name == null || name.length == 0 || !(expreLetras.test(name))){
+			alert("Debe escribir un nombre válido");}
+		else if( name.charAt(0).toUpperCase()!= name.charAt(0)){
+			alert ("Debe colocar la primera letra de nombre en mayúscula");
+		}
+
+		if (lastname == null || lastname.length == 0 || !(expreLetras.test(lastname))){
+			alert("Debe escribir un apellido válido");}
+		else if( lastname.charAt(0).toUpperCase()!= lastname.charAt(0)){
+			alert ("Debe colocar la primera letra de apellido en mayúscula");
+		}
+	//Validacion email y contraseña
+	var expreEmail = /\S+@\S+\.\S+/;
+  if(!(expreEmail.test(email)) || email == null || email.length == 0){
 			alert('Debe escribir un correo válido');
-	}
-  if (password == null || password.length == 0){
+	} else if (password == null || password.length == 0){
 		alert("Debe escribir una contraseña válida");
-  }
-	if(password.toString().length < 6 || password.toString() == "password" ||
+  } else if(password.toString().length < 6 || password.toString() == "password" ||
  			password == "123456" || password == "098754"){
 		alert("Contraseña incorrecta");
 	}
+
   if( bici == null || bici == 0 ) {
     alert("Completar datos en seleccionar bici");
   }
+
 }
