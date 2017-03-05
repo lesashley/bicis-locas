6eddfa9977630dd3b7c7bb901d23bb9ceddf821a
lesashley <lesashley56@gmail.com>
2017-03-05 18:35:30 -0500

    Insertando ultima validación

--- js/main.js
+++ js/main.js
@@ -6,14 +6,13 @@ function validateForm(){
   var bici = document.getElementById("bici").selectedIndex;
   var social = document.getElementById("input-social").value;
 
-	var expreLetras = /([a-zñáéíóú ]+)$/;
+	var expreLetras = /([A-Z]{1}[a-zñáéíóú]\D+)$/;
 //Validaciones en nombre
   	if (name == null || name.length == 0 || !(expreLetras.test(name))){
 			alert("Debe escribir un nombre válido");}
 		else if( name.charAt(0).toUpperCase()!= name.charAt(0)){
 			alert ("Debe colocar la primera letra de nombre en mayúscula");
 		}
-
 		if (lastname == null || lastname.length == 0 || !(expreLetras.test(lastname))){
 			alert("Debe escribir un apellido válido");}
 		else if( lastname.charAt(0).toUpperCase()!= lastname.charAt(0)){
@@ -21,17 +20,18 @@ function validateForm(){
 		}
 	//Validacion email y contraseña
 	var expreEmail = /\S+@\S+\.\S+/;
-  if(!(expreEmail.test(email)) || email == null || email.length == 0){
-			alert('Debe escribir un correo válido');
-	} else if (password == null || password.length == 0){
-		alert("Debe escribir una contraseña válida");
-  } else if(password.toString().length < 6 || password.toString() == "password" ||
- 			password == "123456" || password == "098754"){
-		alert("Contraseña incorrecta");
-	}
+	  if(!(expreEmail.test(email)) || email == null || email.length == 0){
+				alert('Debe escribir un correo válido');
+		}
+		if (password == null || password.length == 0){
+			alert("Debe escribir una contraseña válida");
+	  } else if(password.toString().length < 6 || password.toString() == "password" ||
+	 			password == "123456" || password == "098754"){
+			alert("Contraseña no válida");
+		}
 
-  if( bici == null || bici == 0 ) {
-    alert("Completar datos en seleccionar bici");
-  }
+	  if( bici == null || bici == 0 ) {
+	    alert("Completar datos en seleccionar bici");
+	  }
 
 }
